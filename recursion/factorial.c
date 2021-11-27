d3a5f658b81462b1760b667ebd732c75a79d3f53
Diego Alves <alvessd@live.com>
2021-11-26 22:37:34 -0300

    init useful dev

---
+++ recursion/factorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int factorial(int n) {
+  if (n <= 1) {
+    return 1;
+  }
+
+  return n * factorial(n-1);
+}
+
+int main(void) {
+  int num;
+
+  printf("enter a number: ");
+  scanf("%d", &num);
+
+  printf("%d! = %d\n", num, factorial(num));
+
+  return 0;
+}
